c8db846e4d1810d6c2a258293d53282e11c22c0d
sanket-kogekar <sanket@bhyve.io>
2022-02-05 20:42:23 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3000/",
+      "https://easy-voting.netlify.app/",
+      "http://easy-voting.netlify.app/",
+      "http://easy-voting.netlify.app",
+      "https://easy-voting.netlify.app",
+    ],
+  },
+});
+const port = process.env.PORT || 5000;
+
+let sockets = [];
+let pollQuestion = "";
+
+const getPrecretedQuestion = () => {
+  return pollQuestion;
+};
+
+io.on("connection", (socket) => {
+  socket.join("WaitingRoom");
+  sockets.push(socket);
+
+  socket.on("start-voting", (questionValue) => {
+    pollQuestion = questionValue;
+    socket.to("WaitingRoom").emit("new-question", pollQuestion);
+  });
+
+  socket.on("fetch-pre-created-question", () => {
+    const preCreatedQuestion = getPrecretedQuestion();
+    console.log("preCreatedQuestion", preCreatedQuestion);
+    socket.emit("fetch-pre-created-question", preCreatedQuestion);
+  });
+
+  socket.on("submit-selected-poll-option", (pollAnswerDetails) => {
+    socket.to("WaitingRoom").emit("poll-selection", pollAnswerDetails);
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
